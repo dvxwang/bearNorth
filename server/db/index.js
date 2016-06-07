03d4cdd9bae15fcd5df80801732be308539c9552
jszymanowski <j.szymanowski@gmail.com>
2016-06-07 19:33:30 -0400

    Updated associations for Review, Box models post-merge conflict

--- server/db/index.js
+++ server/db/index.js
@@ -6,22 +6,15 @@ require('./models/products')(db);
 require('./models/reviews')(db);
 require('./models/box')(db);
 
-<<<<<<< HEAD
 var User = db.model('user');
 var Product = db.model('product');
 var Box = db.model('box');
-var Activity = db.model('activity');
-var Type = db.model('type');
 var Review = db.model('review');
 
-Product.belongsTo(Type);
-Activity.hasMany(Type);
-Type.hasMany(Product);
+
 Product.hasMany(Review);
 Review.belongsTo(User);
 Box.belongsToMany(Product, {through: 'BoxProduct'});
 Product.belongsToMany(Box, {through: 'BoxProduct'});
-=======
->>>>>>> master
 
 module.exports = db;
