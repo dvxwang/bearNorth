55f6eacb801c108d3e2c512f5d128418d2554956
Jonathan Rim <jonrim@umich.edu>
2016-06-09 12:33:12 -0400

    merge

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -4,3 +4,12 @@ app.config(function ($stateProvider) {
         templateUrl: 'js/home/home.html'
     });
 });
+
+app.controller('HomeCtrl', function($state,$scope){
+	
+	$scope.goToSurvey = function(activity){
+		var choice = activity.target.firstChild.data;
+		$state.go('survey',{choice: choice});
+	}
+
+})
