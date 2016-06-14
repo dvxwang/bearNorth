2d287bf6239aca9a926864ff5d50c3d08a87ce25
jszymanowski <j.szymanowski@gmail.com>
2016-06-14 12:26:20 -0400

    Reconfigured product route tests

--- tests/server/routes/products-test.js
+++ tests/server/routes/products-test.js
@@ -1,21 +1,20 @@
 var expect = require('chai').expect;
+var supertest = require('supertest');
 
-var Sequelize = require('sequelize');
-var dbURI = 'postgres://localhost:5432/testingfsg';
-var db = new Sequelize(dbURI, {
-    logging: false
-});
-
+var db = require('../../../server/db');
 require('../../../server/db/models/products')(db);
 require('../../../server/db/models/user')(db);
-
-var supertest = require('supertest');
 var Product = db.model('product');
 var User = db.model('user');
 
 describe('Products Route', function () {
 
-  var app;
+  var app, guestAgent, adminAgent;
+  var adminUserDetails = {
+    email: 'obama@email.com',
+    password: 'potus',
+    isAdmin: true
+  }
 
   beforeEach('Sync DB', function () {
     return db.sync({ force: true });
@@ -25,88 +24,87 @@ describe('Products Route', function () {
     app = require('../../../server/app')(db);
   });
 
-	describe('Non-admin requests', function () {
-
-		var guestAgent;
-
-		beforeEach('Create guest agent', function () {
-			guestAgent = supertest.agent(app);
-		});
-
-    beforeEach('Create a product', function (done) {
-      var aProduct = {
-              name: 'A bag',
-              brand: 'North Face',
-              category: 'Sleeping Bag',
-              quantity: 10,
-              purchase_price: 12000,
-              rental_price: 1000,
-              pictureUrl: 'http://i1.wp.com/theverybesttop10.com/wp-content/uploads/2014/04/The-World%E2%80%99s-Top-10-Best-Images-of-Cats-Wearing-Helmets-Made-of-Fruit-2.jpg?resize=470%2C392',
-              description: 'Bestest sleeping bag ever.'
-          },
-          anotherProduct = {
-                  name: 'Another bag',
-                  brand: 'North Face',
-                  category: 'Sleeping Bag',
-                  quantity: 10,
-                  purchase_price: 12000,
-                  rental_price: 1000,
-                  pictureUrl: 'http://i1.wp.com/theverybesttop10.com/wp-content/uploads/2014/04/The-World%E2%80%99s-Top-10-Best-Images-of-Cats-Wearing-Helmets-Made-of-Fruit-2.jpg?resize=470%2C392',
-                  description: 'Bestest sleeping bag ever.'
-              };
-      return Product.bulkCreate([aProduct,anotherProduct])
-      .then(function (products) {
-        done();
-      })
-      .catch(done);
+  beforeEach('Create an admin user', function() {
+    User.create(adminUserDetails)
+    .then( function(adminUser) {
+      console.log('admin user created')
     });
+  });
 
+  beforeEach('Create agents', function(done) {
+    guestAgent = supertest.agent(app);
+    adminAgent = supertest.agent(app);
+    adminAgent.post('/login')
+      .send(adminUserDetails)
+      .end( function(err, res) {
+        console.log('adminAgent logged in')
+        done();
+      });
+  });
+
+  beforeEach('Create products', function (done) {
+    var aProduct = {
+            name: 'A bag',
+            brand: 'North Face',
+            category: 'Sleeping Bag',
+            quantity: 10,
+            purchase_price: 12000,
+            rental_price: 1000,
+            pictureUrl: 'http://i1.wp.com/theverybesttop10.com/wp-content/uploads/2014/04/The-World%E2%80%99s-Top-10-Best-Images-of-Cats-Wearing-Helmets-Made-of-Fruit-2.jpg?resize=470%2C392',
+            description: 'Bestest sleeping bag ever.'
+        },
+        anotherProduct = {
+                name: 'Another bag',
+                brand: 'Big Agnes',
+                category: 'Sleeping Bag',
+                quantity: 10,
+                purchase_price: 11999,
+                rental_price: 1000,
+                pictureUrl: 'http://i1.wp.com/theverybesttop10.com/wp-content/uploads/2014/04/The-World%E2%80%99s-Top-10-Best-Images-of-Cats-Wearing-Helmets-Made-of-Fruit-2.jpg?resize=470%2C392',
+                description: 'The second bestest sleeping bag ever.'
+        };
+    return Product.bulkCreate([aProduct,anotherProduct])
+    .then(function (products) {
+      done();
+    })
+    .catch(done);
+  });
+
+	describe('Guest requests', function () {
 		it('should get all products', function (done) {
 			guestAgent.get('/api/products').end(function(err, res) {
         expect(res.body.length).to.equal(2);
         done();
       })
 		});
-
     it('should get one product', function (done) {
 			guestAgent.get('/api/products/1').end(function(err, res) {
-        console.log(res.body)
-        expect(res.body.name).to.equal('Another bag');
+        expect(res.body.name).to.equal('A bag');
         done();
       })
 		});
+  });
 
+  describe('Admin requests', function () {
+    it('admins should be able to update a product', function (done) {
+      var newProductDetails = {
+        name: 'New bag name'
+      }
+      adminAgent.put('/api/products/1')
+      .send(newProductDetails)
+      .end(function(err, res) {
+        guestAgent.get('/api/products/1').end(function(err, res) {
+          expect(res.body.name).to.equal(newProductDetails.name);
+          done();
+        })
+      })
+    });
+    // it('non-admin user cannot create a product', function (done) {
+		// 	guestAgent.post('/api/products').end(function(err, res) {
+    //     expect(res.status).to.equal(500);
+    //     done();
+    //   })
+		// });
 	});
-});
 
-// 	describe('Authenticated request', function () {
-//
-// 		var loggedInAgent;
-//
-// 		var userInfo = {
-// 			email: 'joe@gmail.com',
-// 			password: 'shoopdawoop'
-// 		};
-//
-// 		beforeEach('Create a user', function (done) {
-// 			return User.create(userInfo).then(function (user) {
-//                 done();
-//             }).catch(done);
-// 		});
-//
-// 		beforeEach('Create loggedIn user agent and authenticate', function (done) {
-// 			loggedInAgent = supertest.agent(app);
-// 			loggedInAgent.post('/login').send(userInfo).end(done);
-// 		});
-//
-// 		it('should get with 200 response and with an array as the body', function (done) {
-// 			loggedInAgent.get('/api/members/secret-stash').expect(200).end(function (err, response) {
-// 				if (err) return done(err);
-// 				expect(response.body).to.be.an('array');
-// 				done();
-// 			});
-// 		});
-//
-// 	});
-//
-// });
+});
